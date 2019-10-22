ef4405c5d079efa44d0aabee3b48caa262a7e043
solomonfrank <solomonfrank73@hotmail.com>
2019-10-22 15:04:20 -0700

    edit user endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,18 +2,14 @@ import User from '../db/model/User';
 import  { respondWithSuccess } from '../helpers/responseHandler';
 
 export const createUser = async (req, res) => {
-   
     
     try {
-        
         const { rows } = await User.init().insert( { ...req.body, created_at: new Date() });
-
-      
         return respondWithSuccess(res, 201, 'user successfully recorded',rows)
         
     } catch (err) {
-        console.log(err.stack)
-      //  return respondWithSuccess(res, 201, 'user successfully recorded',user)
+      
+        return respondWithSuccess(res, 500, 'Internal server error');
     }
   
 }
