f3d1135e7fafc29ee1ddda72d548760b5ba8d1d8
solomonfrank <solomonfrank73@hotmail.com>
2019-11-13 23:20:27 -0800

    modify delete route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import { createUser, editUser, deleteUser, fetchAllUsers }from '../controllers/u
 const router = Router();
 router.post('/api/v1/users', createUserValidation, createUser );
 router.put('/api/v1/users/:userId', validateUserId, editUser);
-router.delete('/api/v1/user/:userId', validateUserId, deleteUser);
+router.delete('/api/v1/users/:userId', validateUserId, deleteUser);
 router.get('/api/v1/users', fetchAllUsers);
 
 export default router;
